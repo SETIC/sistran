35c3a735546d6ca54fee986c2d56a56b00c9fcf7
franklindias <franklindias99@gmail.com>
2016-05-19 16:31:32 -0300

    ajustes heroku

--- demutran/settings.py
+++ demutran/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from decouple import config, Csv
+from dj_database_url import parse as dburl
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -16,12 +18,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-g&y$n_v@uh8@rp*mt!uxevn)nn-4#ku@s3loa3pxk0v=v3pdo'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.246']
+ALLOWED_HOSTS =  config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
 # Application definition
@@ -83,11 +85,11 @@ WSGI_APPLICATION = 'demutran.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_sgg_int',
-        'USER': 'admin_stc',
-        'PASSWORD': 'xdr5tgbhu8ik,',
-        'HOST': '192.168.1.246',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
