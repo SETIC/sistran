cbc3a1e153fa6fd5e769c1b10814c825408de9c7
franklindias <franklindias99@gmail.com>
2016-06-27 17:50:20 -0300

    - paginacao na listagem de veiculos - busca por marca, modelo e placa de veiculo - anteção do local do botão de cadastro de veiculo

--- demutran/sistran/views.py
+++ demutran/sistran/views.py
@@ -19,8 +19,11 @@ from django.db.models import Q
 @login_required
 def home(request):
     taxisCount = Permissao.objects.filter(tipo_concessao='TÁXI').count()
+    alternativoCount = Permissao.objects.filter(tipo_concessao='ALTERNATIVO').count()
+    escolarCount = Permissao.objects.filter(tipo_concessao='ESCOLAR').count()
     permissionariosCount = Proprietario.objects.count()
-    return render(request, 'sistran/dashboard.html', {'taxisCount':taxisCount, 'permissionariosCount':permissionariosCount})
+
+    return render(request, 'sistran/dashboard.html', {'taxisCount':taxisCount, 'alternativoCount':alternativoCount, 'escolarCount':escolarCount, 'permissionariosCount':permissionariosCount})
 
 @login_required
 def permission_denied(request):
@@ -48,7 +51,7 @@ def permissao_list(request, tipo):
         Q(permissao_veiculo__veiculo__placa__icontains=query) |
         Q(proprietario__id__id__id__nome__icontains=query)).order_by('permissao_veiculo__permissao__num_permissao')
 
-    paginator = Paginator(permissoes_list, 10)
+    paginator = Paginator(permissoes_list, 12)
 
     page = request.GET.get('page')
     try:
@@ -401,7 +404,7 @@ def proprietario_list(request):
     proprietarios = Proprietario.objects.filter(
         Q(id__id__id__nome__icontains=query))
 
-    paginator = Paginator(proprietarios, 10)
+    paginator = Paginator(proprietarios, 12)
 
     page = request.GET.get('page')
 
@@ -500,7 +503,18 @@ def proprietario_remove(request, pk):
 @login_required
 def veiculo_list(request):
 
-    veiculos_list = Veiculo.objects.all()
+    query = request.GET.get('q')
+
+    if query is None:
+        query = ''
+
+    veiculos_list = Veiculo.objects.filter(
+        Q(placa__icontains=query) |
+        Q(modelo__icontains=query) |
+        Q(marca__icontains=query)).all()
+
+    print (veiculos_list)
+
     paginator = Paginator(veiculos_list, 12)
 
     page = request.GET.get('page')
@@ -511,7 +525,8 @@ def veiculo_list(request):
     except EmptyPage:
         veiculos = paginator.page(paginator.num_pages)
 
-    return render(request, 'sistran/models/veiculo/veiculo_list.html', {'veiculos': veiculos})
+    return render(request, 'sistran/models/veiculo/veiculo_list.html', {'veiculos': veiculos, 'query':query})
+
 
 @login_required
 def veiculo_detail(request, pk):
